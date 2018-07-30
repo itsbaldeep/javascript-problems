4d659dbb0f6380d809454ebb8edfda0115422a17
Baldeep Singh <38287537+itsbaldeep@users.noreply.github.com>
2018-07-30 20:24:45 +0530

    Added one program

---
+++ remaking-es6-template-literals.js
@@ -0,0 +1,30 @@
+// Let a function named convert which has parameter of template literals
+// 1. literals are the strings
+// 2. args are the values of ${arg}
+const convert = (literals, ...args) => {
+  // We let j to loop through args
+  let j = 0;
+
+  // Simply returing the answer
+  // literals is the array of strings
+  // args is the array of args values
+  return (
+    literals
+      // Mapping each literal(string) with the args value
+      .map(i => i + args[j++])
+      // Splicing to remove the undefined last literal
+      .splice(0, 2)
+      // Joining the array to get single line of result
+      .join("")
+  );
+};
+
+// Example of getting area and perimeter of a square
+// Let side constant to be 10
+const s = 10;
+
+// We log the convert function with template literal as an argument
+console.log(convert`Area is ${s * s} and the Perimeter is ${4 * s}`);
+
+// This logs the same answer as
+// console.log(`Area is ${s*s} and the Perimeter is ${4*s}`);
