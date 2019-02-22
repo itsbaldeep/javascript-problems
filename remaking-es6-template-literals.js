5583fb31712d83b5f5eea97d5e9f6f5349cf33eb
Baldeep Singh <38287537+itsbaldeep@users.noreply.github.com>
2019-02-22 20:38:36 +0530

    Updated the code

--- remaking-es6-template-literals.js
+++ remaking-es6-template-literals.js
@@ -2,21 +2,17 @@
 // 1. literals are the strings
 // 2. args are the values of ${arg}
 const convert = (literals, ...args) => {
-  // We let j to loop through args
-  let j = 0;
+    // We let j to loop through args
+    let j = 0;
 
-  // Simply returing the answer
-  // literals is the array of strings
-  // args is the array of args values
-  return (
-    literals
-      // Mapping each literal(string) with the args value
-      .map(i => i + args[j++])
-      // Splicing to remove the undefined last literal
-      .splice(0, 2)
-      // Joining the array to get single line of result
-      .join("")
-  );
+    // Simply returing the answer
+    // literals is the array of strings
+    // args is the array of args values
+    return literals
+        // Mapping each literal(string) with the args value
+        .map(i => args[j] ? i + args[j++] : i)
+        // Joining the array to get single line of result
+        .join("");
 };
 
 // Example of getting area and perimeter of a square
@@ -24,7 +20,7 @@ const convert = (literals, ...args) => {
 const s = 10;
 
 // We log the convert function with template literal as an argument
-console.log(convert`Area is ${s * s} and the Perimeter is ${4 * s}`);
+console.log(convert`Area is ${s * s} and the Perimeter is ${4 * s}.`);
 
 // This logs the same answer as
 // console.log(`Area is ${s*s} and the Perimeter is ${4*s}`);
